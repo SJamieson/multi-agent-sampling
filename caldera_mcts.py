b7078ac2eee76e89c16640ee885a6e3861904d70
Stewart Jamieson <sjamieson@whoi.edu>
2019-03-31 17:17:52 -0400

    Added: scripts for mcts

---
+++ caldera_mcts.py
@@ -0,0 +1,127 @@
+from mcts import mcts
+from bayes_opt.bayesian_optimization import *
+from copy import deepcopy
+import matplotlib
+import matplotlib.mlab as mlab
+from sklearn.gaussian_process.kernels import RBF
+from sklearn.gaussian_process import GaussianProcessRegressor
+import warnings
+
+
+def caldera_sim_function(x, y):
+    warnings.filterwarnings("ignore", category=matplotlib.cbook.MatplotlibDeprecationWarning)
+    x, y = x / 10.0, y / 10.0
+    z0 = mlab.bivariate_normal(x, y, 10.0, 5.0, 5.0, 0.0)
+    z1 = mlab.bivariate_normal(x, y, 1.0, 2.0, 2.0, 5.0)
+    z2 = mlab.bivariate_normal(x, y, 1.7, 1.7, 8.0, 8.0)
+    return 50000.0 * z0 + 2500.0 * z1 + 5000.0 * z2
+
+
+class RobotState:
+    def __init__(self, x, y, bounds, samples, acq_func, max_depth, step_size, depth=0, base_reward=0, dir='north',
+                 kernel=RBF(length_scale=5)):
+        self.x = x
+        self.y = y
+        self.bounds = bounds
+        self.samples = deepcopy(samples)
+        self.acq_func = acq_func
+        self.depth = depth
+        self.base_reward = base_reward
+        self.max_depth = max_depth
+        self.step_size = step_size
+        self.dir = dir
+
+        self.can_backtrack = False
+        self.y_max = 1
+        self.gp = GaussianProcessRegressor(kernel=kernel, alpha=1e-3, normalize_y=False, n_restarts_optimizer=25,
+                                           random_state=1)
+
+        if len(samples) > 0:
+            xdata = np.vstack(list(self.samples.keys()))
+            ydata = np.vstack(list(self.samples.values()))
+            self.y_max = np.max(ydata)
+            self.gp.fit(xdata, ydata.reshape(-1) / self.y_max)
+
+
+    def set_backtracking(self, val):
+        self.can_backtrack = val
+
+    @staticmethod
+    def _invert_dir(dir):
+        return {'north': 'south', 'south': 'north',
+                'west': 'east', 'east': 'west'}[dir]
+
+    def renew(self, new_samples):
+        return RobotState(self.x, self.y, self.bounds, new_samples, self.acq_func, depth=0, base_reward=0,
+                          max_depth=self.max_depth, step_size=self.step_size, dir=self.dir, kernel=self.gp.kernel)
+
+    def getPossibleActions(self):
+        actions = []
+        if self.x >= self.bounds['x'][0] + self.step_size:
+            actions.append('west')
+        if self.x <= self.bounds['x'][1] - self.step_size:
+            actions.append('east')
+        if self.y >= self.bounds['y'][0] + self.step_size:
+            actions.append('south')
+        if self.y <= self.bounds['y'][1] - self.step_size:
+            actions.append('north')
+        if not self.can_backtrack and len(actions) > 1 and self._invert_dir(self.dir) in actions:
+            actions.remove(self._invert_dir(self.dir))
+        return actions
+
+    def takeAction(self, action):
+        new_samples = deepcopy(self.samples)
+        new_samples[(self.x, self.y)] = self.getReward()
+        new_state = self.renew(new_samples)
+        new_state.depth = self.depth + 1
+        new_state.base_reward = self.base_reward + self.getReward()
+
+        if action == 'west':
+            new_state.x -= self.step_size
+        if action == 'east':
+            new_state.x += self.step_size
+        if action == 'north':
+            new_state.y += self.step_size
+        if action == 'south':
+            new_state.y -= self.step_size
+        return new_state
+
+    def isTerminal(self):
+        global max_depth
+        return self.depth >= self.max_depth
+
+    def getReward(self):
+        reward, = self.acq_func.utility(np.array([self.x, self.y]).reshape(1, -1),
+                                        self.gp, y_max=1)
+        return reward * self.y_max
+
+
+def mcts_state_update(mcts, state, samples, sample_func):
+    mcts.search(initialState=state)
+    state = mcts.getBestChild(mcts.root, 0).state
+    if (state.x, state.y) not in samples:
+        samples[(state.x, state.y)] = sample_func(state.x, state.y)
+    new_robot_state = state.renew(samples)
+    return new_robot_state
+
+
+if __name__ == "__main__":
+    bounds = {'x': (0, 100), 'y': (0, 100)}
+    start = [80, 50]
+    kappa = 2.576
+    xi = 0
+    max_depth = 7
+    num_actions = 150
+    step_size = 4
+    can_backtrack = False
+    acq = 'ucb'
+
+    ucb = UtilityFunction('ucb', kappa=kappa, xi=0)
+    ei = UtilityFunction('ei', kappa=0, xi=0)
+    acq_func = ucb if acq == 'ucb' else ei
+
+    mcts = mcts(timeLimit=max_depth * 300)
+    samples = dict()
+    robot_state = RobotState(*start, bounds, samples, acq_func, max_depth=max_depth)
+    for _ in range(num_actions):
+        mcts_state_update(mcts, robot_state, samples, sample_func=caldera_sim_function)
---
+++ mcts_comparison.py
@@ -0,0 +1,85 @@
+from caldera_mcts import *
+from tqdm import tqdm
+import sys
+import matplotlib
+
+matplotlib.use("TkAgg")
+import matplotlib.pyplot as plt
+from matplotlib import animation
+
+video_length = 12
+
+## Sim parameters
+pbounds = {'x': (0, 100), 'y': (0, 100)}
+start = [80, 50]
+kappa = 2.576
+xi = 0
+max_depth = 7
+num_actions = 180
+fps = num_actions // video_length
+step_size = 4
+can_backtrack = False
+acq = 'ucb'
+filename = 'single-acq={}.{}-{}x{}y-d{}-na{}-ss{}.mp4'.format(acq, kappa if acq == 'ucb' else xi, start[0],
+                                                                   start[1], max_depth, num_actions, step_size)
+
+## MCTS setup
+acq_func = UtilityFunction(acq, kappa=kappa, xi=xi)
+mcts = mcts(timeLimit=max_depth * 300)
+samples = dict()
+robot_state = RobotState(*start, pbounds, samples, acq_func, max_depth, step_size)
+
+## Plotting setup
+delta = 1
+x = np.arange(0, 101.0, delta)
+y = np.arange(0, 101.0, delta)
+X, Y = np.meshgrid(x, y)
+fig = plt.figure()
+ax1 = fig.add_subplot(2, 1, 1, adjustable='box', aspect=1.0)
+ax1.title.set_text('Depth Map')
+CS = plt.contour(X, Y, caldera_sim_function(X, Y), levels=12)
+plt.clabel(CS, inline=1, fontsize=8, fmt='%.3g')
+plt.colorbar(CS, ax=ax1)
+ax2 = fig.add_subplot(2, 1, 2, adjustable='box', aspect=1.0)
+ax2.title.set_text('Acquisition Function')
+xs = np.array([])
+ys = np.array([])
+cbar, marker1, marker2 = None, None, None
+
+t = tqdm(total=num_actions + 1, file=sys.stdout)
+
+
+def update(_):
+    global samples, cbar, marker1, marker2, xs, ys, robot_state, t
+    robot_state = mcts_state_update(mcts, robot_state, samples, sample_func=caldera_sim_function)
+
+    # plt.subplot(211)
+    xs = np.concatenate((xs, [robot_state.x]))
+    ys = np.concatenate((ys, [robot_state.y]))
+    if marker1 is not None:
+        ax1.lines.pop()
+    pos1, = ax1.plot(xs[-2:], ys[-2:], color='k')
+    marker1, = ax1.plot(robot_state.x, robot_state.y, '*b')
+    # plt.pause(0.1)
+
+    # plt.subplot(212)
+    scores = acq_func.utility(np.vstack([X.ravel(), Y.ravel()]).transpose(), robot_state.gp, 0)
+    scores = scores.reshape(X.shape) * robot_state.y_max
+    if marker2 is not None:
+        ax2.lines.pop()
+    im = ax2.imshow(scores, cmap='hot', interpolation='nearest')
+    im.set_clim(vmin=0)
+    if cbar is not None:
+        cbar.remove()
+    cbar = plt.colorbar(im, ax=ax2)
+    pos2, = ax2.plot(xs[-2:], ys[-2:], color='k')
+    marker2, = ax2.plot(robot_state.x, robot_state.y, '*b')
+    ax2.invert_yaxis()
+    # plt.pause(0.1)
+    t.update(n=1)
+    plt.tight_layout()
+    return marker1, marker2, pos1, pos2, im
+
+
+anim = animation.FuncAnimation(fig, update, frames=num_actions, blit=True)
+anim.save(filename, fps=fps)
